c9653965dfaeebce4d7d25080429d262c8b044e0
Florenlis <jpfloren16@gmail.com>
2024-07-05 16:06:41 -0300

    falta pouco

---
+++ src/components/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import { KeyboardAvoidingView, View, Text, TextInput, Alert } from "react-native";
+import { MaterialIcons, Entypo} from "@expo/vector-icons";
+import {styles} from './styles';
+import {colors} from '../../../styles/colors';
+import {ComponentButtonInterface} from '../../../components';
+import { LoginTypes } from "../../navigations/login.navigation";
+
+export
